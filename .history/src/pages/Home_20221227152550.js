bb2d6dfad11f59fc8ae12ec3170490159972c8e4
kennedyowusu <kennediowusu@gmail.com>
2022-12-27 15:26:12 +0000

    nested routes

---
+++ .history/src/pages/Home_20221227152550.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <section className='section'>
+      <h2>Home Page</h2>
+      <Link to='/about' className='btn'> About </Link>
+    </section>
+  );
+};
+export default Home;
